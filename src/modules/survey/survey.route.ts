fd5a4b5dc0e939e2e2b73d2f8843e1d77d861dea
lambojac <adamokennie@gmail.com>
2025-09-04 10:03:45 +0100

    middleware removed from survey route

--- src/modules/survey/survey.route.ts
+++ src/modules/survey/survey.route.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { getNext,answer,fetchAll,fetchAnswered,fetchUnanswered, } from "./survey.controller";
-import { protect } from "../../middlewares/auth";
+
 
 const router = Router();
 
-router.use(protect);
 router.get("/survey/:userId/next", getNext);
 router.post("/survey/:userId/answer", answer);
 router.get("/all", fetchAll);                
